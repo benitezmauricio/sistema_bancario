4d6a02a35e479c8a9cac4235dedbb67f21a49556
LuisMPlus <lx147812345@gmail.com>
2026-09-06 22:55:43 -0300

    feat(models): agregar anotaciones y habilitar el rastreo de auditoria sobre los modelos de cliente y transaccion

--- src/main/java/ar/edu/unju/fi/arquitecturas/cuentabancaria/model/Cliente.java
+++ src/main/java/ar/edu/unju/fi/arquitecturas/cuentabancaria/model/Cliente.java
@@ -1,10 +1,18 @@
 package ar.edu.unju.fi.arquitecturas.cuentabancaria.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.Instant;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,11 +20,30 @@ import lombok.NoArgsConstructor;
 
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
     private String nombre;
+
+    @Column(nullable = false)
     private Integer cuil;
+
+    @Column(nullable = false)
     private String mail;
+
+    @Column(nullable = false)
     private Integer telefono;
+
+    @Column(nullable = false)
     private String direccion;
+
+    @CreatedDate
+    private Instant createdDate;
+
+    @LastModifiedDate
+    private Instant lastModifiedDate;
 
     public void registrarCLiente(){}
 
--- src/main/java/ar/edu/unju/fi/arquitecturas/cuentabancaria/model/Transaccion.java
+++ src/main/java/ar/edu/unju/fi/arquitecturas/cuentabancaria/model/Transaccion.java
@@ -1,23 +1,47 @@
 package ar.edu.unju.fi.arquitecturas.cuentabancaria.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.sql.Time;
+import java.time.Instant;
 import java.util.Date;
 
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 
 public class Transaccion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
     private Date fecha;
     private Time hora;
+    @Column(nullable = false)
     private float monto;
+    @Column(nullable = false)
     private TipoTransaccion tipo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="estado_cuenta", nullable = false)
     private EstadoTransaccion estado_transaccion;
+
+    @CreatedDate
+    private Instant createdDate;
+
+    @LastModifiedDate
+    private Instant lastModifiedDate;
 
     public void consultarTransaccion() {}
 
